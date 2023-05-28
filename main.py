f3d080fa4023e569f45695f34e4f945dcd7347af
danilPorkchop15 <danilkostin150705@gmail.com>
2023-05-29 01:09:01 +0300

    Burger Clicker / pre-alpha / UI-only

---
+++ main.py
@@ -0,0 +1,11 @@
+import sys
+from interface import *
+import sys
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    BurgerClicker = QtWidgets.QMainWindow()
+    ui = Ui_BurgerClicker()
+    ui.setupUi(BurgerClicker)
+    BurgerClicker.show()
+    sys.exit(app.exec_())
