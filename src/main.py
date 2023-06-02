bcaa74039b7b80c7ffb413330e317c5fbd2386ac
danilPorkchop15 <danilkostin150705@gmail.com>
2023-06-02 23:09:10 +0300

    comments

--- src/main.py
+++ src/main.py
@@ -5,7 +5,10 @@ import sys
 
 
 if __name__ == "__main__":
-	
+
+	# Создание экземпляра класса QApplication, представляющего приложение.
 	app = QtWidgets.QApplication(sys.argv)
+	# Создание экземпляра класса Ui_BurgerClickerEvents, представляющего главное окно приложения.
 	window = Ui_BurgerClickerEvents()
+	# Запуск главного цикла обработки событий приложения и завершение программы при его завершении.
 	sys.exit(app.exec_())
